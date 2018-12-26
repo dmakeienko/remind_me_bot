48fe3be6383e0e0e1e3bc26264e54de477dc87de
Denis Makeenko <d.makeenko@mobidev.biz>
2018-12-26 13:23:07 +0200

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
+
+
+TOKEN = ""
+TIME=datetime.datetime.now().time()
+
+def start(bot, update):
+  update.message.reply_text("I'm a bot, Nice to meet you!")
+  
+def convert_uppercase(bot, update):
+  update.message.reply_text(update.message.text.upper())
+
+
+def time(bot, update):
+  update.message.reply_text(TIME)
+
+
+def main():
+  # Create Updater object and attach dispatcher to it
+  updater = Updater(TOKEN)
+  dispatcher = updater.dispatcher
+  print("Bot started")
+
+  # Add command handler to dispatcher
+  start_handler = CommandHandler('start',start)
+  upper_case = MessageHandler(Filters.text, convert_uppercase)
+  dispatcher.add_handler(start_handler)
+  dispatcher.add_handler(upper_case)
+
+  time_handler = CommandHandler('time',time)
+  dispatcher.add_handler(time_handler)
+
+  # Start the bot
+  updater.start_polling()
+
+  # Run the bot until you press Ctrl-C
+  updater.idle()
+
+
+if __name__ == '__main__':
+  main()
