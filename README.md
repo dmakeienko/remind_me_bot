5bc19a8b9e969fbf09f17da2dacd2e75b3d589c4
Denis Makeenko <d.makeenko@mobidev.biz>
2018-12-27 15:06:33 +0200

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Info
+
+## Installing
+
+This bot created with [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import datetime
+import logging
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
 TOKEN = ""
