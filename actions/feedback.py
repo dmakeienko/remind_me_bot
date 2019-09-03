c6eadc16773eaaba34eb3888e2e72c5f1d5e3c1b
Denys Makeienko <denys.makeienko@gmail.com>
2019-09-04 00:10:07 +0300

    Fix feedback text, added reply

--- actions/feedback.py
+++ actions/feedback.py
@@ -2,5 +2,7 @@ from dotenv import load_dotenv
 import os
 
 def feedback(bot, update, args):
-    feedback = ''.join(args)
+    feedback_text = ' '.join(args)
+    feedback = 'You received feedback from user: ⚠️\n' + feedback_text
     bot.send_message(chat_id=os.environ['ADMIN_CHAT_ID'], text=feedback)
+    bot.send_message(chat_id=update.message.chat_id, text='Thank you for feedback! Here is a 🍩 for you ☺️.')
