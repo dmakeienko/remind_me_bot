4fa3867207907df4e248cbfc74c2f3158261a096
Denys Makeienko <denys.makeienko@gmail.com>
2019-09-06 22:21:30 +0300

    fix list command, change reminds time

--- actions/list_reminds.py
+++ actions/list_reminds.py
@@ -1,15 +1,13 @@
 from db.database import get_reminds
 from datetime import datetime
-from utils.constants import DATETIME_FORMAT
+from utils.constants import DATETIME_FORMAT, LIST_ALL_FLAG, LIST_MONTH_FLAG, LIST_WEEK_FLAG
 
 def list_reminds(bot, update, args):
   my_reminds = ''
   user_chat_id = update.message.chat_id
   remind_status = ''
-  interval = ''.join(args)
-  print(interval)
-  if get_reminds(user_chat_id, 'all'):
-    print(interval + 'interval')
+  interval = ''.join(args).lower()
+  try:
     for r in get_reminds(user_chat_id, interval):
       if r['expired'] == False:
         remind_status = '🕖'
@@ -19,7 +17,9 @@ def list_reminds(bot, update, args):
         remind_status = '✅'
       time = datetime.strptime(r['remind_time'], DATETIME_FORMAT).strftime('%d.%m %H:%M')
       my_reminds += f"🗓 {r['id']}: ⏰ {time} 📌 {r['remind_text']}: {remind_status}\n"
-  else: 
-    my_reminds = 'Oops 😯, you have no reminds yet.'
-
-  bot.send_message(chat_id=update.message.chat_id, text=my_reminds)
+    bot.send_message(chat_id=update.message.chat_id, text=my_reminds)
+  except:
+    if interval == '':
+      interval = "today"
+    my_reminds = f"Oops 😯, you have no reminds for {interval}."
+    bot.send_message(chat_id=update.message.chat_id, text=my_reminds)
--- db/database.py
+++ db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, cast, Da
 from sqlalchemy import desc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from utils.constants import DATETIME_FORMAT, LAST_REMIND_TIME
+from utils.constants import DATETIME_FORMAT, LAST_REMIND_TIME, LIST_ALL_FLAG, LIST_MONTH_FLAG, LIST_WEEK_FLAG
 import datetime 
 import json
 import logging
@@ -67,15 +67,21 @@ def get_reminds(user_chat_id, interval):
     logger.info("Getting reminds...")
     session = Session()
     if interval == '':
-        print('nothing')
         reminds_list = session.query(Remind).filter_by(chat_id=user_chat_id).\
             filter(cast(Remind.remind_time, Date) == datetime.datetime.today().date() ).\
-                order_by(Remind.id).all()
+                order_by(Remind.remind_time).all()
         json_data = json.loads(json.dumps(reminds_list, cls=RemindEncoder, indent=4))
-    elif interval == 'all':
-        print('all')
+    elif interval == LIST_ALL_FLAG:
         # Select all reminds, user is defined by chat_id
-        reminds_list = session.query(Remind).filter_by(chat_id=user_chat_id).order_by(Remind.id).all()
+        reminds_list = session.query(Remind).filter_by(chat_id=user_chat_id).order_by(Remind.remind_time).all()
+    elif interval ==  LIST_WEEK_FLAG:
+        today = datetime.datetime.today()
+        weekday = today.weekday()
+        mon = today - datetime.timedelta(days=weekday)
+        sun = today + datetime.timedelta(days=(6 - weekday))
+        reminds_list = session.query(Remind).filter_by(chat_id=user_chat_id).\
+            filter(cast(Remind.remind_time, Date) >= mon, cast(Remind.remind_time, Date) <= sun ).\
+                order_by(Remind.remind_time).all()
 
     json_data = json.loads(json.dumps(reminds_list, cls=RemindEncoder, indent=4))
     # Close session
