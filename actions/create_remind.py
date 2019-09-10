d18c6ee8cef6cee97e402e4c3d75c573f69d7340
Denys Makeienko <denys.makeienko@gmail.com>
2019-09-11 00:13:19 +0300

    add aliases

--- actions/create_remind.py
+++ actions/create_remind.py
@@ -1,4 +1,5 @@
 from db.database import create
+import datetime
 
 def create_remind(bot, update, args):
     user_chat_id = update.message.chat_id
@@ -7,12 +8,47 @@ def create_remind(bot, update, args):
         time_remind = user_message[0] + " " + user_message[1]
         
         reminder_text = user_message[2] 
-        remind = "Your remind 📆 has been set!"
+        remind = "Your remind has been set! 📆"
         create(user_chat_id, time_remind, reminder_text, False)
         bot.send_message(chat_id=user_chat_id, text=remind)
     except IndexError:
-        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can`t create remind. Maybe text is missing? 🤔')
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe text is missing? 🤔')
     except ValueError:
-        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can`t create remind. Maybe something wrong with date/time? 🤔')
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe something wrong with date/time? 🤔')
     except Exception:
         bot.send_message(chat_id=user_chat_id, text='Looks like you are trying to set remind to past date 🤔. Are you sure?')
+
+
+
+def create_today(bot, update, args):
+    try:
+        user_chat_id = update.message.chat_id
+        user_message = ' '.join(args).split(" ", 1)
+        tomorrow_date = datetime.date.today().strftime('%Y-%m-%d')
+        remind_full_time = tomorrow_date + " " + user_message[0]
+        remind_text = user_message[1]
+        remind = f"Remind 📌 \"{remind_text}\" scheduled for {user_message[0]} today 📆"
+        create(user_chat_id, remind_full_time, remind_text, False)
+        bot.send_message(chat_id=user_chat_id, text=remind)   
+    except IndexError:
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe text is missing? 🤔')
+    except ValueError:
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe something wrong with time? 🤔')
+
+
+
+def create_tomorrow(bot, update, args):
+    try:
+        user_chat_id = update.message.chat_id
+        user_message = ' '.join(args).split(" ", 1)
+        tomorrow_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        remind_full_time = tomorrow_date + " " + user_message[0]
+        remind_text = user_message[1]
+        remind = f"Remind 📌 \"{remind_text}\" scheduled for {user_message[0]} tomorrow 📆"
+        create(user_chat_id, remind_full_time, remind_text, False)
+        bot.send_message(chat_id=user_chat_id, text=remind)   
+    except IndexError:
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe text is missing? 🤔')
+    except ValueError:
+        bot.send_message(chat_id=user_chat_id, text='Oops 😯, can\'t create remind. Maybe something wrong with time? 🤔')
+
