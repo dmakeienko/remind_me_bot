92e9968c3b25d253185e9d549739ea02c4822f58
Denys Makeienko <denys.makeienko@gmail.com>
2019-09-23 13:45:02 +0300

    adding buttons to remind

--- actions/menu.py
+++ actions/menu.py
@@ -2,13 +2,13 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from utils.menu import build_menu
 from actions.close_remind import close_remind
 
-def remind_button_menu(bot, update):
+def remind_button_menu(bot, chat_id):
     button_list = [
         InlineKeyboardButton("Mark as done", callback_data='done'),
         InlineKeyboardButton("Postpone for 30 min", callback_data='postpone')
     ]
     reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
-    bot.send_message(chat_id=update.message.chat_id, text='What to do with remind?', reply_markup=reply_markup)
+    bot.send_message(chat_id=chat_id, text='What to do with remind?', reply_markup=reply_markup)
 
 
 
